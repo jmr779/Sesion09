4ff2e67f960098151e6e8dd1a07134fbb33a1d35
jmr779 <jmr779@inlumine.ual.es>
2018-06-14 17:43:00 +0200

    Unido registro y login en ingreso aplicacion

--- login/src/main/java/ventanas/Ingreso_aplicacion.java
+++ login/src/main/java/ventanas/Ingreso_aplicacion.java
@@ -6,8 +6,10 @@ public class Ingreso_aplicacion extends Ingreso_aplicacion_ventana implements Vi
 	public Usuario _unnamed_Usuario_;
 	public Registro _unnamed_Registro_;
 	public IniciarSesion ini = new IniciarSesion();
+	Registro reg = new Registro();
 	
 	public Ingreso_aplicacion() {
-		addComponent(ini);
+		horizontalLayout.addComponent(ini);
+		horizontalLayout.addComponent(reg);
 	}
 }
--- login/src/main/java/ventanas/Registro.java
+++ login/src/main/java/ventanas/Registro.java
@@ -2,7 +2,7 @@ package ventanas;
 
 import java.util.Date;
 
-public class Registro {
+public class Registro extends Registro_ventana{
 	public Ingreso_aplicacion _unnamed_Ingreso_aplicacion_;
 
 	public void registrarse(String aEmail, String aPass, String aNombre, Date aFechaCreacion, Date aFechaUltimoAcceso) {
