6eec0a5d7872c5b139df4a4aa2962d9c7c2eee95
AlfonsoCabreraGimenez <acg125@inlumine.ual.es>
2018-06-15 11:57:06 +0200

    fhk

--- login/src/main/java/ventanas/Pag_Administrador.java
+++ login/src/main/java/ventanas/Pag_Administrador.java
@@ -1,6 +1,38 @@
 package ventanas;
 
-public class Pag_Administrador {
+import java.util.List;
+
+import org.orm.PersistentException;
+
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Grid;
+import com.vaadin.ui.NativeButton;
+
+import bbdd.*;
+
+public class Pag_Administrador  extends Pag_Administrador_ventana{
 	public ListarUsuarios _unnamed_ListarUsuarios_;
 	public ModificarDatosUserListado _unnamed_ModificarDatosUserListado_;
+	public BD_Registrado regis = new BD_Registrado();
+	
+	public Pag_Administrador() throws PersistentException {
+		
+		Grid <bbdd.Registrado> grid = new Grid<>();
+		List<bbdd.Registrado> lista = regis.cargarUsuarios();
+		grid.setItems(lista);
+		
+		grid.addColumn(bbdd.Registrado::getNombre);
+		/////////grid.addColumn(new NativeButton("Boton"));
+		
+		bListar.addClickListener(new ClickListener() {
+			
+			public void buttonClick(ClickEvent event) {
+				
+				
+				
+			}
+		});
+	}
 }
